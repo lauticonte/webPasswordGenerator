245f3bb170df4364400f89abde97ffcb337bb172
Lautaro Conte <lauty.conte@gmail.com>
2021-08-04 16:14:03 -0300

    Cambios css

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ console.clear();
 // Fill : The trailing color that you see when you drag the slider.
 // background : Default Range Slider Background
 const sliderProps = {
-	fill: "#0B1EDF",
+	fill: "var(--color-gradient-end)",
 	background: "rgba(255, 255, 255, 0.214)",
 };
 
